7df64a0d800adb5b29d44c731cae0d5f5a8ca6fa
JimWu <a0283137@gmail.com>
2022-08-08 16:24:59 +0800

    完成院線片編輯電影功能

---
+++ api/edit_movie.php
@@ -0,0 +1,31 @@
+<?php 
+include_once "../base.php";
+$row=$Movie->find($_POST['id']);
+
+
+//如果沒有東西傳進來  就用原本的值
+
+if(!empty($_FILES['trailer']['tmp_name'])){
+    $_POST['trailer']=$_FILES['trailer']['name'];
+    move_uploaded_file($_FILES['trailer']['tmp_name'],'../upload/'.$_FILES['trailer']['name']);
+}else{
+    $_POST['trailer']=$row['trailer'];
+}
+if(!empty($_FILES['poster']['tmp_name'])){
+
+    $_POST['poster']=$_FILES['poster']['name'];
+    move_uploaded_file($_FILES['poster']['tmp_name'],'../upload/'.$_FILES['poster']['name']);
+}else{
+    $_POST['poster']=$row['poster'];
+}
+
+$_POST['ondate']=$_POST['year']."-".$_POST['month']."-".$_POST['day'];
+unset($_POST['year'],$_POST['month'],$_POST['day']);
+$_POST['sh']=$row['sh'];
+$_POST['rank']=$row['rank'];
+
+
+$Movie->save($_POST);
+
+to("../back.php?do=movie");
+?> 
