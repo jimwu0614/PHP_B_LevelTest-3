6b5f7efadc1e290beb71723077d3dd4178ca9bf2
JimWu <a0283137@gmail.com>
2022-08-12 16:35:23 +0800

    建立預告片海報版面配置css

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
-﻿<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+﻿<?php include "./base.php"?>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <!-- saved from url=(0047)? -->
 <html xmlns="http://www.w3.org/1999/xhtml">
 
@@ -29,7 +31,7 @@
         <div id="mm">
             <?php
                 $do=$_GET['do']??'main';
-                $file = "./front/".$do."/php";
+                $file = "./front/".$do.".php";
                 if(file_exists($file)){
                     include $file;
                 }else{
