2c7c5156d1d2d6a285de7118e24dc70ab6f2f27d
JimWu <a0283137@gmail.com>
2022-08-19 13:07:48 +0800

    完成畫位畫面下方的電影資訊資料取得

--- front/order.php
+++ front/order.php
@@ -1,4 +1,4 @@
-<?php  $selectedMovieId=$_GET['id']??0 ;?>
+<?php $selectedMovieId = $_GET['id'] ?? 0; ?>
 <div id="order">
 
     <h2 class="ct">線上訂票</h2>
@@ -13,13 +13,13 @@
         <tr>
             <td>日期：</td>
             <td>
-            <select name="date" id="date"></select>
+                <select name="date" id="date"></select>
             </td>
         </tr>
         <tr>
             <td>場次：</td>
             <td>
-            <select name="session" id="session"></select>
+                <select name="session" id="session"></select>
             </td>
         </tr>
     </table>
@@ -30,60 +30,127 @@
 </div>
 
 <div id="booking" style="display:none">
-劃位
+    劃位
 
 </div>
 
 <script>
-    $("#movie").load("./api/movie_list.php",{id:<?=$selectedMovieId;?>},()=>{
-        let movie=$("#movie").val();
+    let info = {
+        movieId: 0,
+        movieName: '',
+        date: '',
+        sessionId: 0,
+        session: '',
+    }
+    $("#movie").load("./api/movie_list.php", {
+        id: <?= $selectedMovieId; ?>
+    }, () => {
+        let movie = $("#movie").val();
         getDate(movie)
     })
 
-    $("#movie").on('change',function(){
-        let movie=$(this).val();
+    $("#movie").on('change', function() {
+        let movie = $(this).val();
         getDate(movie)
 
     })
 
-    $("#date").on('change',function(){
-        let movie=$("#movie").val();
-        let date=$(this).val();
-        getSession(movie,date)
+    $("#date").on('change', function() {
+        let movie = $("#movie").val();
+        let date = $(this).val();
+        getSession(movie, date)
     })
 
-    function getDate(movie){
+    function getDate(movie) {
 
-        $("#date").load("./api/date_list.php",{movie},()=>{
-            let date=$("#date").val()
-            getSession(movie,date)
+        $("#date").load("./api/date_list.php", {
+            movie
+        }, () => {
+            let date = $("#date").val()
+            getSession(movie, date)
         })
     }
 
-    function getSession(movie,date){
+    function getSession(movie, date) {
         // console.log(movie,date);
-        $("#session").load("./api/session_list.php",{movie,date},()=>{
+        $("#session").load("./api/session_list.php", {
+            movie,
+            date
+        }, () => {
 
         })
     }
-    
-    function booking(){
+
+    function booking() {
         $("#order").hide();
         $("#booking").show();
-        $.get("./api/get_booking.php",(seats)=>{
-            $("#booking").html(seats)
-            $(".seat input").on("change",function(){
-                if($(this).parent().hasClass("empty")){
+        $.get("./api/get_booking.php", (seats) => {
+            $("#booking").html(seats);
+            updateInfo();
+            setSeatEvent();
+
+        })
+    }
+
+    function updateInfo(){
+        info.movieId=$("#movie").val()
+        info.movieName=$("#movie option:selected").text()
+        info.date=$("#date").val()
+        info.sessionId=$("#session").val()
+        info.session=$("#session option:selected").text().split(" ")[0]
+    }
+
+
+    function setSeatEvents() {
+        let seats = new Array();
+
+        $("#movieName").text(info.movieName)
+        $("#dateStr").text(info.date)
+        $("#sessionName").text(info.session)
+        
+        $(".seat input").on("change", function() {
+            let num = $(this).val()
+
+            if ($(this).prop('checked')) {
+                if (seats.length >= 4) {
+                    alert("最多只能勾選四張票")
+                    $(this).prop('checked', false)
+                } else {
+                    seats.push(num)
                     $(this).parent().removeClass("empty")
                     $(this).parent().addClass("checked")
-                }else{
-                    $(this).parent().removeClass("checked")
-                    $(this).parent().addClass("empty")
+                }
+            } else {
+                seats.splice(seats.indexOf(num), 1);
+                $(this).parent().removeClass("checked")
+                $(this).parent().addClass("empty")
+            }
 
-                    }
-                })
+            $("#tickets").text(seats.length)
+
         })
     }
 
 
-</script> 
+
+
+
+
+
+
+
+    // function setSeatEvent() {
+    //     $(".seat input").on("change", function() {
+    //         let num = $(this).val();
+    /* 增減CLASS 達到圖片更換(有人沒人)的效果 */
+    //         if ($(this).parent().hasClass("empty")) {
+    //             $(this).parent().removeClass("empty")
+    //             $(this).parent().addClass("checked")
+    //         } else {
+    //             $(this).parent().removeClass("checked")
+    //             $(this).parent().addClass("empty")
+
+    //         }
+    //     })
+    // }
+</script>
